d83cf71fc24cd6ef10456f04f7191a8bb7926e76
phanidevlancer <phanidevlancer@gmail.com>
2023-09-25 20:29:19 +0530

    Added constants

--- app/src/main/java/com/example/dairyapp_multi_module_udemy/MainActivity.kt
+++ app/src/main/java/com/example/dairyapp_multi_module_udemy/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.dairyapp_multi_module_udemy.ui.theme.DairyApp_Multi_Module_UdemyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
 
         }
---
+++ app/src/main/java/com/example/dairyapp_multi_module_udemy/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.dairyapp_multi_module_udemy.utils
+
+object Constants {
+    const val MONGO_APP_ID = "mydairyapp-gfede"
+}
