68bc299ddac1afb6b18ac9155a22bf3b9550f235
Phanindra <phani.2205@gmail.com>
2023-10-07 12:04:15 +0530

    new changes

--- app/src/main/java/com/example/dairyapp_multi_module_udemy/presentation/components/DairyHolder.kt
+++ app/src/main/java/com/example/dairyapp_multi_module_udemy/presentation/components/DairyHolder.kt
@@ -1,68 +1,133 @@
 package com.example.dairyapp_multi_module_udemy.presentation.components
 
-import androidx.compose.foundation.layout.Column
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.toLowerCase
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import java.time.LocalDate
+import com.example.dairyapp_multi_module_udemy.model.Dairy
+import com.example.dairyapp_multi_module_udemy.model.Mood
+import com.example.dairyapp_multi_module_udemy.ui.theme.Elevation
+import com.example.dairyapp_multi_module_udemy.utils.toInstant
+import io.realm.kotlin.ext.realmListOf
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
+@SuppressLint("UnrememberedMutableInteractionSource")
 @Composable
-fun DateHeader(localDate: LocalDate){
-    Row(verticalAlignment = Alignment.CenterVertically){
-        Column(horizontalAlignment = Alignment.End) {
-            Text(
-                text = String.format("%02d",localDate.dayOfMonth),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
+fun DairyHolder(dairy: Dairy, onClick: (String) -> Unit) {
+    val localDensity = LocalDensity.current
+    var componentHeight by remember { mutableStateOf(0.dp) }
 
-            Text(
-                text = localDate.dayOfWeek.toString().take(3),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
-        }
-
+    Row(modifier = Modifier.clickable(
+        indication = null,
+        interactionSource = MutableInteractionSource()
+    ) { onClick(dairy._id.toString()) }) {
         Spacer(modifier = Modifier.width(14.dp))
+        Surface(
+            modifier = Modifier
+                .width(2.dp)
+                .height(14.dp), tonalElevation =
+            Elevation.Level1
+        ) {
 
-        Column(horizontalAlignment = Alignment.Start) {
+        }
+        Spacer(modifier = Modifier.width(20.dp))
+        Surface(
+            modifier = Modifier
+                .clip(shape = Shapes().medium)
+                .onGloballyPositioned {
+                    componentHeight = with(localDensity) { it.size.height.toDp() }
+                },
+            tonalElevation = Elevation.Level5
+        ) {
+            DiaryHeader(moodName = dairy.mood, time = dairy.date.toInstant())
             Text(
-                text = localDate.month.toString().toLowerCase().replaceFirstChar { it.titlecase() },
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
-
-            Text(
-                text = "${localDate.year}",
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
-                    fontWeight = FontWeight.Light
-                )
+                modifier = Modifier.padding(all = 14.dp),
+                text = dairy.description,
+                style = TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                maxLines = 4,
+                overflow = TextOverflow.Ellipsis
             )
         }
-
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ShowDateHolder(){
-    DateHeader(localDate = LocalDate.now())
+fun DiaryHeader(moodName: String, time: Instant) {
+    val mood by remember { mutableStateOf(Mood.valueOf(moodName)) }
+    val formatter = remember {
+        DateTimeFormatter.ofPattern("hh:mm a", Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(mood.containerColor)
+            .padding(horizontal = 14.dp, vertical = 7.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier.size(18.dp),
+                painter = painterResource(id = mood.icon),
+                contentDescription = "Mood Icon",
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+            Text(
+                text = mood.name,
+                color = mood.contentColor,
+                style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+            )
+        }
+        Text(
+            text = formatter.format(time),
+            color = mood.contentColor,
+            style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+        )
+    }
 }
+
+@Composable
+@Preview
+fun ShowDairyHolder() {
+    DairyHolder(dairy = Dairy().apply {
+        title = "My Diary"
+        description =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        mood = Mood.Happy.name
+        images = realmListOf("", "")
+    }, onClick = {})
+}
+
+
